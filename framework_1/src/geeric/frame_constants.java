23010aa978e67ec9a5c063c4a0b83dc8b2f89051
HP BS 661 <HP BS 661@LAPTOP-JKFQ777J>
2022-12-07 22:36:17 +0530

    commit

---
+++ framework_1/src/geeric/frame_constants.java
@@ -0,0 +1,13 @@
+package geeric;
+
+public interface frame_constants 
+{
+String gecko_KEY="webdriver.gecko.driver";
+String gecko_value="./Softwares/geckodriver.exe";
+
+String chrome_KEY="webdriver.chrome.driver";
+String chrome_value="./Softwares/chromedriver.exe";
+
+String url="https://www.facebook.com";
+}
+		
